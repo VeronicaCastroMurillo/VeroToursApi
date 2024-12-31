d7fc09a84f966f9f42dfb70c978a857b4775a7ed
VeronicaCastroMurillo <veronicacastromurillo@gmail.com>
2024-12-30 20:20:37 -0600

    Se creo el DTO de usuario

---
+++ VeroToursApi/DTOS/UsuarioDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace VeroToursApi.DTOS
+{
+    public class UsuarioDTO
+    {
+        public int UsuarioId { get; set; }
+
+        public string Contraseña { get; set; } = null!;
+
+        public string Nombre { get; set; } = null!;
+
+        public string Apellido { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string? Telefono { get; set; }
+
+        public DateTime? FechaRegistro { get; set; }
+    }
+}
